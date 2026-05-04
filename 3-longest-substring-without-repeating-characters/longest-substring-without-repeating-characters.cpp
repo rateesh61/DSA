b05b251a543d8eb6decf61e49c59b2569e3f81ae
Rateesh Bhargava <160837385+rateesh61@users.noreply.github.com>
2026-05-05 00:59:24 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (90.06%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-     int n=s.length();
-    int maxlen=0,l=0;
-    for(int i=0;i<n;i++){
-    int hash[256]={0};    
-        for(int j=i;j<n;j++){
-            if(hash[s[j]]==1) break;
-            l=j-i+1;
-            maxlen=max(maxlen,l);
-            hash[s[j]]=1;
+        int n=s.length();
+        int l=0,r=0,maxlen=0,len=0;
+        int hash[256];
+        for(int i = 0; i < 256; i++) hash[i] = -1;
+        while(r<n){
+             if(hash[s[r]]!=-1){
+                if(hash[s[r]]>=l){
+                    l=hash[s[r]]+1;
+                }
+             }   
+             len=r-l+1;
+             maxlen=max(maxlen,len);
+             hash[s[r]]=r;
+             r++;
         }
-     }
-     return maxlen;
-  }
+        return maxlen;
+   }
 };
