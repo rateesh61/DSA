0aca9598fdc234d8dda9572fbf523bb8e996f985
Rateesh Bhargava <160837385+rateesh61@users.noreply.github.com>
2026-09-11 13:48:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (71.83%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -8,14 +8,13 @@ public:
             x = 1 / x;
             N = -N;
         }
-
-        while (N > 0) {
-            if (N % 2 == 1) {
-                ans *= x;
+        while(N>0){
+            if(N%2==1){
+                ans*=x;
             }
 
-            x *= x;
-            N /= 2;
+            x*=x;
+            N/=2;
         }
 
         return ans;
