0f29069052d68c3ea98277eae68cd1aeeb006974
Rateesh Bhargava <160837385+rateesh61@users.noreply.github.com>
2026-09-23 19:52:51 +0530

    Time: 5 ms (27.54%) | Memory: 14.4 MB (32.05%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void solve(vector<int>& candidates,int i,vector<int>& comb, vector<vector<int>>& ans, int target){
-        if(target<0  || i==candidates.size()) return;
+    void solve(vector<int>& candidates,int start,vector<int>& comb, int target){
+        if(target<0  || start==candidates.size()) return;
         if(target==0){
             ans.push_back(comb);
             return;
         }
-
-        comb.push_back(candidates[i]);
-        solve(candidates,i,comb,ans,target-candidates[i]);     //single
-      //  solve(candidates,i,comb,ans,target-candidates[i]);     //multiple
-        comb.pop_back();
-        solve(candidates,i+1,comb,ans,target);     //exclusion on the right side of the tree
-
+        for(int i=start;i<candidates.size();i++)
+        {
+          comb.push_back(candidates[i]);
+          solve(candidates,i,comb,target-candidates[i]);     //single
+                       //  solve(candidates,i,comb,ans,target-candidates[i]);     //multiple
+          comb.pop_back();
+        }
 
     }
 
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        ans.clear();
         vector<int> comb;
-        solve(candidates,0,comb,ans,target);
+        solve(candidates,0,comb,target);
         return ans;
     }
 };
